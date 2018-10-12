2c632586191984a5cd7e1121c80a9dd2fdc0a546
LeonardMBP <leonard@net90-dhcp-36.sfc.keio.ac.jp>
2018-10-12 16:53:26 +0900

    removed alias 2 uploading Xcode project

---
+++ sfcbustimer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sfcbustimer
+//
+//  Created by Leonard Choo on 2018/10/12.
+//  Copyright © 2018 team-sfcbustimer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
