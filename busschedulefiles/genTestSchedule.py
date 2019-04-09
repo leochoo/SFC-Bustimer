aa613a39a717d88fe903faab8295728a7b664b39
LeonardMBP <leonard@Leonards-MacBook-Pro.local>
2019-04-09 16:27:23 +0900

    tested on Sol's device, auto sigining

--- busschedulefiles/genTestSchedule.py
+++ busschedulefiles/genTestSchedule.py
@@ -12,18 +12,18 @@ testData["sfcsho"]["weekday"] = []
 
 
 for h in range(0,24):
-    for m in range(0,60):
-        busData = {
-            "hour": h,
-            "min": m,
-            "type": None,
-            "rotary": False
-        }
-        testData["sfcsho"]["weekday"].append(busData)
+	for m in range(0,60):
+		busData = {
+			"hour": h,
+			"min": m,
+			"type": None,
+			"rotary": False
+		}
+		testData["sfcsho"]["weekday"].append(busData)
 
 # print(testData)
 # print(json.dumps(testData))
 
 with open('testData.json', 'w') as outfile:
-    json.dump(testData, outfile, indent=4)
+	json.dump(testData, outfile, indent=4)
 
